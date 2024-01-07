4c950ac1b067b49633a722abfcd7f0c3ac3a3f3d
Hector Morgado <e3.corest@outlook.com>
2024-01-06 19:09:56 -0500

    Se agregan los archivos de trabajo, primera clase

---
+++ script.ts
@@ -0,0 +1,29 @@
+// los tipos de datos usados en javascript son usados en type
+
+// extension de typescript en vsc para ver errores pretty typescript errors
+
+// los tipos de datos se puede colocar su tipo tan manual como typescript lo puede hace automaticamente
+
+// inferir es laccion que realiza type script para tipar un dato dependiendo el valor que se le carga
+
+const nombre = 'hector'
+let nombres: string = 'juan';
+
+const numero = 2
+let numeros: number = 34
+
+const nova = null;
+const varios: null = null
+
+const nodef = undefined
+let unico: undefined = undefined
+
+// dato any = cualquier tipo de dato y que ignore el tipado de typescript
+
+const anyValue: any = 'valor'
+
+// unknown = es cuando no se sabe que tipo de dato es 
+
+const nosabe: unknown = 'general'
+
+// 28:01
